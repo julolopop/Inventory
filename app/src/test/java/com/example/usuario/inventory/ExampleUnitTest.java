e91b0409601fe611dd167d74b22432477f74ba26
julolopop <julolopop@gmail.com>
2017-11-08 14:42:56 +0100

    pruebas unitarias

--- app/src/test/java/com/example/usuario/inventory/ExampleUnitTest.java
+++ app/src/test/java/com/example/usuario/inventory/ExampleUnitTest.java
@@ -11,7 +11,33 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void SingIn_isCorrect() throws Exception {
+        //assertEquals(4, 2 + 2);
+
+        //1. el usuario introduce usuario/mail
+
+        //2. el usuario introduce una contraseña
+
+        //3. el usuario exista en la base de datos  (Repository)
+
+        //4. Que la contraseña sea correcta
+
+    }
+
+    @Test
+    public void SingUp_isCorrect() throws Exception {
+        //assertEquals(4, 2 + 2);
+
+        //1. el usuario introduce usuario
+
+        //2. el usuario introduce una contraseña
+
+        //3. el usuario no exista en la base de datos  (Repository)
+
+        //4. La contraseña tiene que tener al menos 6 caracteres
+
+        //5. Que la contraseña sea correcta doble ab/ar ar/ab
+
+        //6. que mail no existe en la base de datos (Repository)
     }
 }
