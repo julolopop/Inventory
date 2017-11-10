e5d969ba2d471e58e7b75eed158edd477447f3dc
julolopop <julolopop@gmail.com>
2017-11-10 14:03:32 +0100

    pruebas usuario y contraseña

--- app/src/androidTest/java/com/example/usuario/inventory/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/usuario/inventory/ExampleInstrumentedTest.java
@@ -1,7 +1,7 @@
 package com.example.usuario.inventory;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
+
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.After;
@@ -9,7 +9,16 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -35,23 +44,36 @@ public class ExampleInstrumentedTest {
 
     }
 
+
+    private void CheckSnackBarDisplayByMessage(int error) {
+        onView(withText(error)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+    }
+
     //1. el usuario introduce usuario/mail (instrumentales)
     @Test
     public void IsUserEmpty() throws  Exception{
+        onView(withId(R.id.btnSignIn)).perform(click());
+        CheckSnackBarDisplayByMessage(R.string.ErrorUser);
 
     }
+
 
 
     //2. el usuario introduce una contraseña(instrumentales)
     @Test
     public void IsPasswordEmpty() throws  Exception{
-
+        onView(withId(R.id.btnSignIn)).perform(click());
+        CheckSnackBarDisplayByMessage(R.string.ErrorPassword);
     }
 
     //4. La contraseña tiene que tener al menos 6 caracteres(instrumentales)
     @Test
     public void IsPasswordWith6char() throws  Exception{
+        onView(withId(R.id.edtUser)).perform(typeText("Juanma"),closeSoftKeyboard());
+        onView(withId(R.id.edtPassword)).perform(typeText("Jua"),closeSoftKeyboard());
+        onView(withId(R.id.btnSignIn)).perform(click());
 
+        CheckSnackBarDisplayByMessage(R.string.ErrorpasswordLength);
     }
     //5. Que la contraseña sea correcta doble ab/ar ar/ab(instrumentales)
     @Test
