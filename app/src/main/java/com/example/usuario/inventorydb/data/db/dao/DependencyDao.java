939cc395ebf84ee2aa9dedff2a13fc3e4e2eacd6
julolopop <julolopop@gmail.com>
2018-01-24 14:40:54 +0100

    mirar otras personas

--- app/src/main/java/com/example/usuario/inventorydb/data/db/dao/DependencyDao.java
+++ app/src/main/java/com/example/usuario/inventorydb/data/db/dao/DependencyDao.java
@@ -4,6 +4,10 @@ import android.database.Cursor;
 
 import com.example.usuario.inventorydb.data.db.model.InventoryContract;
 import com.example.usuario.inventorydb.data.db.model.InventoryOpenHelper;
+import com.example.usuario.inventorydb.pojo.Dependency;
+
+import java.util.ArrayList;
+import java.util.Observable;
 
 /**
  * Created by usuario on 22/01/18.
@@ -11,10 +15,25 @@ import com.example.usuario.inventorydb.data.db.model.InventoryOpenHelper;
 
 public class DependencyDao {
 
-    public Cursor loadAll(){
-        return InventoryOpenHelper.newInstance().openDatabase().query(InventoryContract.DependencyEntry.TABLE_NAME,
+
+
+    public ArrayList<Dependency> loadAll(){
+        ArrayList<Dependency> arrayList = new ArrayList<>();
+
+        Cursor c = InventoryOpenHelper.newInstance().openDatabase().query(InventoryContract.DependencyEntry.TABLE_NAME,
                 InventoryContract.DependencyEntry.ALL_COLUMNS,
                 null,null,null,null,
                 InventoryContract.DependencyEntry.DEFAULT_ORDER);
+        if (c != null) {
+            while (c.moveToNext()) {
+                arrayList.add(new Dependency(c.getInt(0), c.getString(1),
+                        c.getString(2), c.getString(3),c.getString(4)));
+            }
+        }
+
+        return arrayList;
+
     }
+
+
 }
--- app/src/main/java/com/example/usuario/inventorydb/data/db/repository/DependencyRepository.java
+++ app/src/main/java/com/example/usuario/inventorydb/data/db/repository/DependencyRepository.java
@@ -3,12 +3,9 @@ package com.example.usuario.inventorydb.data.db.repository;
 import android.database.Cursor;
 
 import com.example.usuario.inventorydb.data.db.dao.DependencyDao;
-import com.example.usuario.inventorydb.data.db.model.InventoryOpenHelper;
 import com.example.usuario.inventorydb.pojo.Dependency;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
 
 /**
  * @Aurtor Juan Manuel Diaz Ortiz
@@ -53,25 +50,10 @@ public class DependencyRepository {
     }
 
     public ArrayList<Dependency> getDependencies() {
-        dependencies.clear();
-        Cursor c = getDependencyDao();
-
-        if (c != null) {
-            while (c.moveToNext()) {
-                addDependency(new Dependency(c.getInt(0), c.getString(1),
-                        c.getString(2), c.getString(3),c.getString(4)));
-            }
-        }
-
-        return dependencies;
-    }
-
-    public Cursor getDependencyDao(){
         return dependencyDao.loadAll();
     }
 
     public void editDependency(Dependency dependencia) {
-
 
     }
 
